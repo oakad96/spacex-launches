590096b36ae2fb95dd80eee7dc8d3eeef5558537
Onur Akad <is.onurakad@gmail.com>
2023-05-17 02:01:02 +0300

    loaded api and changed homepage component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Hero from "@/components/Hero";
+import LaunchCard from "@/components/LaunchCard";
 
 export default function Home() {
   return (
     <>
-      <Hero />
+      <LaunchCard />
     </>
   );
 }
