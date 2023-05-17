56e12f2e38c4be8e94e88051f29fa9cfa2202aed
Onur Akad <is.onurakad@gmail.com>
2023-05-17 04:13:52 +0300

    added image carousel

--- components/CrewMembers.jsx
+++ components/CrewMembers.jsx
@@ -20,7 +20,7 @@ const CrewMembers = (props) => {
     fetchCrew();
   }, [crewIds]);
 
-  return (
+  return crew?.length !== 0 ? (
     <div>
       <h4 className="font-bold mb-4">Crew Members:</h4>
       <ul className="flex flex-col gap-4">
@@ -30,13 +30,18 @@ const CrewMembers = (props) => {
               <img
                 src={member?.image}
                 alt={member?.name}
-                className=" w-12 rounded-lg"
+                className="w-12 rounded-lg"
               />
               <span className="my-auto">{member?.name}</span>
             </div>
           </li>
         ))}
       </ul>
+    </div>
+  ) : (
+    <div>
+      <h4 className="font-bold mb-2 lg:mb-4">Crew Members:</h4>
+      <p>No crew members information on this launch.</p>
     </div>
   );
 };
